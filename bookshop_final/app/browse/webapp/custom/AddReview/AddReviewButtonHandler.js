5df6eabfad9d9fcddffcc1eefb2f6a3095cdae56
I328923 <krishma.bhatia@sap.com>
2021-11-30 18:12:22 +0530

    part12 complete

--- bookshop_final/app/browse/webapp/custom/AddReview/AddReviewButtonHandler.js
+++ bookshop_final/app/browse/webapp/custom/AddReview/AddReviewButtonHandler.js
@@ -1,10 +1,19 @@
 sap.ui.define(
-    ["sap/m/MessageBox"],
-    function (MessageBox) {
+    ["sap/ui/core/Fragment"],
+    function (Fragment) {
         "use strict";
         return {
-            onPress: function () {
-                MessageBox.show("button Pressed!");
+            openDialog: async function () {
+                const oBooklistPage = sap.ui.getCore().byId("bookshop_final::BooksList");
+                if (!this.oAddReviewDialog) {
+                    this.sReviewDialogId = `${oBooklistPage.getId()}-AddReviewDialog`;
+                    this.oAddReviewDialog = await Fragment.load({
+                        id: this.sReviewDialogId,
+                        name: "bookshop_final.custom.AddReview.AddReviewDialog",
+                    });
+                    oBooklistPage.addDependent(this.oAddReviewDialog);
+                }
+                this.oAddReviewDialog.open();
             },
         };
     });
